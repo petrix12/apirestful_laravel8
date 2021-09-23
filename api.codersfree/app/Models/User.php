559eb37085ae58d812d6e978f6f91af7fff7ddaf
Pedro Bazó <bazo.pedro@gmail.com>
2021-09-23 14:53:51 -0400

    Video 59: Instalar Laravel Permission

--- api.codersfree/app/Models/User.php
+++ api.codersfree/app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 //use Laravel\Sanctum\HasApiTokens;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, ApiTrait;
+    use HasApiTokens, HasFactory, Notifiable, ApiTrait, HasRoles;
 
     /**
      * The attributes that are mass assignable.
